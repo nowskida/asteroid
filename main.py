af55247021073084229909657fd6ed826a741f3f
dzolnowski <nowskida@gmail.com>
2024-12-01 23:15:51 +0100

    circles

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill('#000000')
         pygame.display.flip()
+
+        # limt the framerate  to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
