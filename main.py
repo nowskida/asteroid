0132c7d395f46d40c0d3039812a460fed88c23c3
dzolnowski <nowskida@gmail.com>
2024-12-01 22:39:50 +0100

    minor changes

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allows us to use code from
+# the open-source pygame library
+# throught this file
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
